a879bcefa978ce2c2e181c9de2e6c0951c78f8a0
nicorellius <nicorellius@gmail.com>
2014-08-12 13:47:40 -0700

    obscured password in db dump script

--- db_dump.sh
+++ db_dump.sh
@@ -2,9 +2,11 @@
 
 # db dump script using lamp stack
 
+password=$1
+
 dump_cmd="/home/nick/dev/lamp/mysql/bin/mysqldump"
 user="root"
-password="Shreding57"
+password="${password}"
 db_name=$1
 
 $dump_cmd -u $user -p$password $db_name > "${db_name}.sql"
